e629e46177e821194b6c138f0519e8a554c43841
Felix <luxyteam404@gmail.com>
2025-11-27 19:04:40 -0600

    x

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,15 +11,15 @@ module.exports = {
       max_memory_restart: '1G',
       env: {
         NODE_ENV: 'production',
-        PORT: 3000
+        PORT: 3001
       },
       env_development: {
         NODE_ENV: 'development',
-        PORT: 3000
+        PORT: 3001
       },
       env_production: {
         NODE_ENV: 'production',
-        PORT: 3000
+        PORT: 3001
       },
       error_file: './logs/pm2-error.log',
       out_file: './logs/pm2-out.log',
